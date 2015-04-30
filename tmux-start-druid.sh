64a3c0730e469381caea16651e87323a1b50573a
Rusty Phillips <rusty.phillips@gmail.com>
2015-04-30 20:11:51 +0000

    Added zookeeper to the tmux server start.

--- tmux-start-druid.sh
+++ tmux-start-druid.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+tmux new-window -n zookeeper -c /opt/kafka 'sudo ./bin/zookeeper-server-start.sh config/zookeeper.properties'
+tmux new-window -n kafka -c /opt/kafka 'sudo ./bin/kafka-server-start.sh config/server.properties'
 tmux new-window -n druid-coordinator -c /srv/druid/current 'sudo HADOOP_USER_NAME=rustyp java -Xmx256m -Duser.timezone=UTC -Dfile.encoding=UTF-8 -classpath config/_common:config/coordinator:lib/*:../hadoop_from_altiscale/* io.druid.cli.Main server coordinator'
 tmux new-window -n druid-historical -c /srv/druid/current 'sudo HADOOP_USER_NAME=rustyp java -Xmx256m -Duser.timezone=UTC -Dfile.encoding=UTF-8 -classpath config/_common:config/historical:lib/*:../hadoop_from_altiscale/* io.druid.cli.Main server historical' 
 tmux new-window -n druid-realtime -c /srv/druid/current 'sudo HADOOP_USER_NAME=rustyp java -Xmx512m -Duser.timezone=UTC -Dfile.encoding=UTF-8 -Ddruid.realtime.specFile=examples/wikipedia/wikipedia_realtime.spec -classpath config/_common:config/realtime:lib/*:../hadoop_from_altiscale/* io.druid.cli.Main server realtime'
