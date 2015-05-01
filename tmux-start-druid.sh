43d92647d88728587c70b88a764e36a6f600a60c
Rusty Phillips <rusty.phillips@gmail.com>
2015-05-01 16:37:54 +0000

    Added druid and kafka starter scripts.

--- tmux-start-druid.sh
+++ tmux-start-druid.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-tmux new-window -n zookeeper -c /opt/kafka 'sudo ./bin/zookeeper-server-start.sh config/zookeeper.properties'
-tmux new-window -n kafka -c /opt/kafka 'sudo ./bin/kafka-server-start.sh config/server.properties'
-tmux new-window -n druid-coordinator -c /srv/druid/current 'sudo HADOOP_USER_NAME=rustyp java -Xmx256m -Duser.timezone=UTC -Dfile.encoding=UTF-8 -classpath config/_common:config/coordinator:lib/*:../hadoop_from_altiscale/* io.druid.cli.Main server coordinator'
-tmux new-window -n druid-historical -c /srv/druid/current 'sudo HADOOP_USER_NAME=rustyp java -Xmx256m -Duser.timezone=UTC -Dfile.encoding=UTF-8 -classpath config/_common:config/historical:lib/*:../hadoop_from_altiscale/* io.druid.cli.Main server historical' 
-tmux new-window -n druid-realtime -c /srv/druid/current 'sudo HADOOP_USER_NAME=rustyp java -Xmx512m -Duser.timezone=UTC -Dfile.encoding=UTF-8 -Ddruid.realtime.specFile=examples/wikipedia/wikipedia_realtime.spec -classpath config/_common:config/realtime:lib/*:../hadoop_from_altiscale/* io.druid.cli.Main server realtime'
-tmux new-window -n druid-broker -c /srv/druid/current 'sudo HADOOP_USER_NAME=rustyp java -Xmx256m -Duser.timezone=UTC -Dfile.encoding=UTF-8 -classpath config/_common:config/broker:lib/*:../hadoop_from_altiscale/* io.druid.cli.Main server broker'
-tmux new-window -n druid-index -c /srv/druid/current 'sudo HADOOP_USER_NAME=rustyp java -Xmx2g -Duser.timezone=UTC -Dfile.encoding=UTF-8 -classpath config/_common:config/overlord:lib/*:../hadoop_from_altiscale/* io.druid.cli.Main server overlord'
+USE_TMUX=1
+
+DRUID_CLASSPATH="config/_common:lib/*:../hadoop_from_altiscale/*"
+DRUID_CMD_START="sudo HADOOP_USER_NAME=rustyp java -Xmx512m -Duser.timezone=UTC -Dfile.encoding=UTF-8"
+DRUID_MAIN_CLASS="io.druid.cli.Main"
+DRUID_DIR="/srv/druid/current"
+DRUID_PARTS=(coordinator historical realtime broker overlord)
+
+function tmux_or_background {
+ #tmux send-keys -t $1 C-z
+  eval $([ "$USE_TMUX" ] && echo "tmux new-window -c $2 -n $1 '$3'" || echo "$3 &"  )
+}
+
+for i in "${DRUID_PARTS[@]}"
+do
+  tmux_or_background "druid-$i" $DRUID_DIR "$DRUID_CMD_START -classpath config/$i:$DRUID_CLASSPATH $DRUID_MAIN_CLASS server $i" 
+done
